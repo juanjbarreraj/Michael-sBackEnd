c8c54c5837bbcc535223737685dfa7ff3a907395
JuanBarrera0305 <120711482+JuanBarrera0305@users.noreply.github.com>
2026-03-04 12:46:38 -0500

    fixed db

--- db.js
+++ db.js
@@ -1,11 +1,25 @@
-const Pool = require("pg").Pool;
+const { Pool } = require("pg");
 
-const pool = new Pool({
-  user: "postgres",
-  host: "127.0.0.1",
-  database: "library",
-  password: process.env.PGPASSWORD,   // in .env
-  port: 2222,
-});
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // Production (Render)
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  
+  // Local development
+  pool = new Pool({
+    user: "postgres",
+    host: "127.0.0.1",
+    database: "library",
+    password: process.env.PGPASSWORD,
+    port: 2222,
+  });
+}
 
 module.exports = pool;
